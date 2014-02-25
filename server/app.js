680a5f445fb64bf9886373db020063a159719f07
Brian Scaturro and Jason Sich <jasich+scaturrob@gmail.com>
2014-02-24 21:46:08 -0500

    setup dummy api

---
+++ server/app.js
@@ -0,0 +1,41 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/../dist'));
+app.locals.site = 'http://localhost:' + app.get('port');
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//ensure all responses are hal+json
+app.use(function(req, res, next) {
+  if (/^\/api/.test(req.path)) {
+    res.set('Content-Type', 'application/json');
+  }
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.render('application');
+});
+app.get('/api', routes.index);
+app.get('/api/topic', routes.topic);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+exports.index = function(req, res){
+  res.render('index');
+};
+
+exports.topic = function(req, res){
+  res.render('topic');
+};
